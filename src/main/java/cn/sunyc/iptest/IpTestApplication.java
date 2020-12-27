1452a98d80ee8fbef31e7c9e8aa18a814eefb5f7
18531630385 <672208496@qq.com>
2020-12-27 15:48:27 +0800

    修改启动类

--- src/main/java/cn/sunyc/iptest/IpTestApplication.java
+++ src/main/java/cn/sunyc/iptest/IpTestApplication.java
@@ -2,12 +2,19 @@ package cn.sunyc.iptest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class IpTestApplication {
+public class IpTestApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(IpTestApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(IpTestApplication.class);
     }
 
 }
